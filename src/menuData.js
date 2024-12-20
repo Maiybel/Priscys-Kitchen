800f93a699e72f9e7b588245383e60d335357d57
Ezema Mabel <ezemamabel57@gmail.com>
2024-12-20 15:51:44 -0400

    add menuData.js file

---
+++ src/menuData.js
@@ -0,0 +1,475 @@
+export const categories = [
+  {
+    categoryName: "Soup",
+    categoryImage: {
+      url: "/Afang soup.jpg",
+      alt: "Soup Category",
+    },
+  },
+  {
+    categoryName: "Stew/Sauce",
+    categoryImage: {
+      url: "/stew.jpg",
+      alt: "Stew Category",
+    },
+  },
+  {
+    categoryName: "Rice",
+    categoryImage: {
+      url: "/fried rice.jpg",
+      alt: "Rice Category",
+    },
+  },
+  {
+    categoryName: "Food",
+    categoryImage: {
+      url: "/Spaghetti.jpg",
+      alt: "Food Category",
+    },
+  },
+];
+
+export const products = [
+  {
+    productName: "Afang Soup",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/Afang soup.jpg",
+      alt: "Product Image",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "2 Litres", price: 10000 },
+      { type: "3 Litres", price: 1500 },
+      { type: "5 Litres", price: 25000 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Fried Rice",
+    productCategory: {
+      categoryName: "Rice",
+      categoryImage: {
+        url: "/jollof rice.jpg",
+        alt: "Rice Image",
+      },
+    },
+    productImage: {
+      url: "/fried rice.jpg",
+      alt: "Product Image",
+    },
+    productPrice: 150,
+    variations: [
+      { type: "2 Litres", price: 10000 },
+      { type: "3 Litres", price: 1500 },
+      { type: "5 Litres", price: 25000 },
+    ],
+    productDescription: "Tasty pastries",
+  },
+  {
+    productName: "Banga Soup",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/Banga soup.webp",
+      alt: "Banga soup",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Bitterleaf Soup",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/Bitterleaf soup.jpg",
+      alt: "Bitterleaf",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Chicken Sauce",
+    productCategory: {
+      categoryName: "Stew/Sauce",
+      categoryImage: {
+        url: "/stew.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/chicken sauce.jpg",
+      alt: "Chicken Sauce",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Egusi Soup",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/Egusi soup.jpg",
+      alt: "Egusi Soup",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "2 Litres", price: 10000 },
+      { type: "3 Litres", price: 1500 },
+      { type: "5 Litres", price: 25000 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Pounded Yam and Egusi soup",
+    productCategory: {
+      categoryName: "Food",
+      categoryImage: {
+        url: "/Spaghetti.jpg",
+        alt: "Food",
+      },
+    },
+    productImage: {
+      url: "/Esugi and swallow.jpg",
+      alt: "Egusi soup and pouded yam",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Fried Rice",
+    productCategory: {
+      categoryName: "Food",
+      categoryImage: {
+        url: "/Spaghetti.jpg",
+        alt: "Food",
+      },
+    },
+    productImage: {
+      url: "/fried rice.jpg",
+      alt: "Fried Rice",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Fried Rice",
+    productCategory: {
+      categoryName: "Food",
+      categoryImage: {
+        url: "/Spaghetti.jpg",
+        alt: "Rice",
+      },
+    },
+    productImage: {
+      url: "/fried rice.jpg",
+      alt: "Fried Rice",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Ogbono Soup",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/ogbono soup.jpeg",
+      alt: "Ogbono Soup",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "2 Litres", price: 10000 },
+      { type: "3 Litres", price: 1500 },
+      { type: "5 Litres", price: 25000 },
+    ],
+    productDescription: "Delicious bread",
+  },
+
+  {
+    productName: "Eforinro",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/Eforinro.jpg",
+      alt: "Eforinro",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Oha Soup",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/Oha soup.jpg",
+      alt: "Oha Soup",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Okro Soup",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/Okro soup 2.jpg",
+      alt: "Okro Soup",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Okro Soup",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/okro-soup.jpeg",
+      alt: "Okro Soup",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Rice and Vegetable Sauce",
+    productCategory: {
+      categoryName: "Food",
+      categoryImage: {
+        url: "/Spaghetti.jpg",
+        alt: "Spaghetti",
+      },
+    },
+    productImage: {
+      url: "/Rice and vegetable stew.jpg",
+      alt: "Rice and Vegetable Sauce",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Snail Stew",
+    productCategory: {
+      categoryName: "Stew/Sauce",
+      categoryImage: {
+        url: "/stew.jpg",
+        alt: "Stew",
+      },
+    },
+    productImage: {
+      url: "/Snail stew.jpg",
+      alt: "Snail Stew",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Spaghetti",
+    productCategory: {
+      categoryName: "Food",
+      categoryImage: {
+        url: "/Spaghetti.jpg",
+        alt: "Spaghetti",
+      },
+    },
+    productImage: {
+      url: "/Spaghetti.jpg",
+      alt: "Spaghetti",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Stew",
+    productCategory: {
+      categoryName: "Stew/Sauce",
+      categoryImage: {
+        url: "/stew.jpg",
+        alt: "Stew",
+      },
+    },
+    productImage: {
+      url: "/stew.jpg",
+      alt: "Stew",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "White Soup",
+    productCategory: {
+      categoryName: "Soup",
+      categoryImage: {
+        url: "/Afang soup.jpg",
+        alt: "Afang Soup",
+      },
+    },
+    productImage: {
+      url: "/White soup.jpg",
+      alt: "White Soup",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Yam and Egg Sauce",
+    productCategory: {
+      categoryName: "Food",
+      categoryImage: {
+        url: "/Spaghetti.jpg",
+        alt: "Spaghetti",
+      },
+    },
+    productImage: {
+      url: "/Yam and eggsauce.jpg",
+      alt: "Yam and Egg Sauce",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+  {
+    productName: "Jollof Rice",
+    productCategory: {
+      categoryName: "Rice",
+      categoryImage: {
+        url: "/jollof rice.jpg",
+        alt: "Jollof Rice",
+      },
+    },
+    productImage: {
+      url: "/Jollofrice.jpg",
+      alt: "Jollof Rice",
+    },
+    productPrice: 100,
+    variations: [
+      { type: "Mini", price: 300 },
+      { type: "Midi", price: 500 },
+      { type: "Maxi", price: 700 },
+    ],
+    productDescription: "Delicious bread",
+  },
+];
